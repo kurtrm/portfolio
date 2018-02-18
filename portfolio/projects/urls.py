35d767005c99eacd1b1b474a527c8ddbcb16f4fa
kurt <kurtrm@gmail.com>
2018-02-18 10:33:08 -0800

    Made

--- portfolio/projects/urls.py
+++ portfolio/projects/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from projects.views import (NetworkView,
+from projects.views import (ProjectsListView,
+                            NetworkView,
                             TreeView)
 
 
 urlpatterns = [
+    url(r'^$', ProjectsListView.as_view(), name='projects'),
     url(r'^network_graph/$', NetworkView.as_view(), name='network_graph'),
     url(r'^decision_tree/$', TreeView.as_view(), name='decision_tree')
 ]
